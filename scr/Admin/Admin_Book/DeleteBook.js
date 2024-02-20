b5a226a0ed5e12046435ebe344538e13e3cfb223
NamOfNames <ckn0203@gmail.com>
2024-02-20 19:30:13 +0700

    fix-admin

--- scr/Admin/Admin_Book/DeleteBook.js
+++ scr/Admin/Admin_Book/DeleteBook.js
@@ -2,7 +2,7 @@ import {
   ref,
   remove,
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-import { dbrt } from "./firebase.js";
+import { dbrt } from "../firebase.js";
 
 export function deleteBook(id) {
   remove(ref(dbrt, `Books/${id}`))
